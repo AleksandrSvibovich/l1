d05b46428bc05c9b2987f79d59b9a16f7de6cd8e
Aleksandr Svibovich <Aleksandr_Svibovich@epam.com>
2018-12-05 17:22:50 +0400

    added new Phone Book app, and ChangeEvent app

---
+++ src/H_SWING/Phone_book/PhoneBookFrame.java
@@ -0,0 +1,18 @@
+package H_SWING.Phone_book;
+
+import javax.swing.*;
+
+/**
+ * Created by Aleksandr_Svibovich on 12/5/2018.
+ */
+public class PhoneBookFrame extends JFrame {
+    private JLabel result;
+    private JTextField textField;
+    private JCheckBox register;
+    private JRadioButton searchFullMatchText;
+    private JRadioButton searchPartOfText;
+
+    public PhoneBookFrame(){
+        
+    }
+}
