780503b12992d65e6adaaad182847522db04ae01
fox_alex5303@yahoo.com <fox_alex5303@yahoo.com>
2019-02-07 21:22:12 +0400

    changed package names

--- src/com/my/projectc/game/Application.java
+++ src/com/my/projectc/game/Application.java
@@ -8,8 +8,7 @@ import com.my.projectc.game.view.View;
  * Created by Aleksandr_Svibovich on 1/25/2019.
  */
 public class Application implements Runnable {
-    private static final int HEIGHT = 800;
-    private static final int WIDTH = 740;
+
 
     public static void main(String[] args) {
         new Application().run();
@@ -17,14 +16,6 @@ public class Application implements Runnable {
 
     @Override
     public void run() {
-//                Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
-//                FrameGameOfLife frame = new FrameGameOfLife(HEIGHT,WIDTH);
-//                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//                frame.setSize(WIDTH,HEIGHT);
-//                frame.setLocation(dimension.width/2 - WIDTH/2, dimension.height/2 - HEIGHT/2);
-//                frame.setTitle("Version 1.0.0");
-//                frame.setResizable(true);
-//                frame.setVisible(true);
         Controller controller = new Controller(new View(), new Model());
         controller.run();
     }
--- src/com/my/projectc/game/view/View.java
+++ src/com/my/projectc/game/view/View.java
@@ -2,10 +2,24 @@ package com.my.projectc.game.view;
 
 import com.my.projectc.game.model.Model;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class View {
+    private static final int HEIGHT = 800;
+    private static final int WIDTH = 740;
     private Model model;
+
     public View() {
         // initialize with some constants
+        Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
+        FrameGameOfLife frame = new FrameGameOfLife(HEIGHT, WIDTH);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(WIDTH, HEIGHT);
+        frame.setLocation(dimension.width / 2 - WIDTH / 2, dimension.height / 2 - HEIGHT / 2);
+        frame.setTitle("Version 1.0.0");
+        frame.setResizable(true);
+        frame.setVisible(true);
     }
 
     public Model getModel() {
